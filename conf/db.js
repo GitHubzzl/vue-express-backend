c2fb1a3a60b8d5e1b4353353f1c3e89303bf4f91
pengpeng <351724109@qq.com>
2019-05-20 18:39:30 +0800

    create by 2019-05-20

---
+++ conf/db.js
@@ -0,0 +1,11 @@
+// conf/db.js
+// MySQL数据库联接配置
+module.exports = {
+    mysql: {
+        host: 'localhost', 
+        user: 'root',
+        password: '000000',
+        database:'express',
+        port: 3306
+    }
+};
